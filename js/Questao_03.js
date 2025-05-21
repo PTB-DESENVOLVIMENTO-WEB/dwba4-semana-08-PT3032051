54ddd9d65c737175f6c90541d8e39c533150fd95
Rogério Santos Barbosa <DevSemCash@gmail.com>
2025-05-20 21:28:51 -0300

    Update Questao_03.js

--- js/Questao_03.js
+++ js/Questao_03.js
@@ -1,1 +1,13 @@
+function mostrarDadosAluno(nome, nota1, nota2) {
+  let n1 = nota1 * 0.6;
+  let n2 = nota2 * 0.4;
+  let media = n1 + n2;
+
+  alert(`Aluno: ${nome}\nNota 1: ${nota1}\nNota 2: ${nota2}`);
+  alert(`Nota final de ${nome}: ${media.toFixed(2)}`);
+}
+
+mostrarDadosAluno("João da Silva", 7, 8.5);
+mostrarDadosAluno("Maria Oliveira", 9, 6.5);
+mostrarDadosAluno("Carlos Souza", 5, 7);
 
